11422d05dadf93bff73117199d06afa1adbfed1d
tribt <bienthanhtri@gmail.com>
2022-10-26 10:16:04 +0700

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from '~/components/Layout';
+import { Fragment } from 'react';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route) => {
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+
+            const Page = route.component;
+            return (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import { HeaderOnly } from '~/components/Layout';
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+const publicRoutes = [
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile, layout: HeaderOnly },
+  { path: '/upload', component: Upload, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
