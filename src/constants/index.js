188c7655a3d76709d79af7ed61020c708a77fd5a
tribt <bienthanhtri@gmail.com>
2022-11-02 10:26:38 +0700

    define route link to constant

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+// define all constant of routes
+export const routeConfig = {
+  home: '/',
+  following: '/following',
+  profile: '/@:nickname',
+  upload: '/upload',
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ import Following from '~/pages/Following';
 import Home from '~/pages/Home';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import { routeConfig } from '~/constants';
 
 const publicRoutes = [
-  { path: '/', component: Home },
-  { path: '/following', component: Following },
-  { path: '/@:nickname', component: Profile, layout: HeaderOnly },
-  { path: '/upload', component: Upload, layout: null },
+  { path: routeConfig.home, component: Home },
+  { path: routeConfig.following, component: Following },
+  { path: routeConfig.profile, component: Profile, layout: HeaderOnly },
+  { path: routeConfig.upload, component: Upload, layout: null },
 ];
 
 const privateRoutes = [];
