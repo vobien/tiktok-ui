4ff20dd4150407dc9157dc0b30edef712cdd89a8
tribt <bienthanhtri@gmail.com>
2022-11-02 16:21:15 +0700

    use .env

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+// console.log(process.env);
+
 const axiosClient = axios.create({
-  baseURL: 'https://tiktok.fullstack.edu.vn/api/',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export default axiosClient;
